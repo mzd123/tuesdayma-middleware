42d94deed3405513b77cbb978ef82da3dc459b1b
zhoudi.mazd <tuesdayma@163.com>
2021-03-10 13:54:19 +0800

    sharding-jdbc

--- src/main/java/com/tuesdayma/middleware/sharding/controller/ShardingController.java
+++ src/main/java/com/tuesdayma/middleware/sharding/controller/ShardingController.java
@@ -1,5 +1,6 @@
 package com.tuesdayma.middleware.sharding.controller;
 
+import com.alibaba.fastjson.JSON;
 import com.tuesdayma.middleware.sharding.service.ShardingService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,8 +16,35 @@ public class ShardingController {
     @Resource
     private ShardingService shardingService;
 
+    /**
+     * 分表分库
+     */
     @RequestMapping("/sharding/insert")
     public void insert() {
         shardingService.insert();
+    }
+
+    /**
+     * 分表分库
+     */
+    @RequestMapping("/sharding/get")
+    public String get() {
+        return shardingService.get();
+    }
+
+    /**
+     * 读写分离
+     */
+    @RequestMapping("/sharding/insert/goods")
+    public void insertGoods() {
+        shardingService.insertGoods();
+    }
+
+    /**
+     * 读写分离
+     */
+    @RequestMapping("/sharding/get/goods")
+    public String getGoods() {
+        return shardingService.getGoods();
     }
 }
--- src/main/java/com/tuesdayma/middleware/sharding/service/ShardingService.java
+++ src/main/java/com/tuesdayma/middleware/sharding/service/ShardingService.java
@@ -1,6 +1,10 @@
 package com.tuesdayma.middleware.sharding.service;
 
+import com.alibaba.fastjson.JSON;
+import com.tuesdayma.middleware.sharding.dal.entity.Goods;
 import com.tuesdayma.middleware.sharding.dal.entity.MyOrder;
+import com.tuesdayma.middleware.sharding.dal.entity.MyOrderCriteria;
+import com.tuesdayma.middleware.sharding.dal.mapper.GoodsMapper;
 import com.tuesdayma.middleware.sharding.dal.mapper.MyOrderMapper;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +19,8 @@ import java.util.UUID;
 public class ShardingService {
     @Resource
     private MyOrderMapper myOrderMapper;
+    @Resource
+    private GoodsMapper goodsMapper;
 
     public void insert() {
         MyOrder order = new MyOrder();
@@ -23,4 +29,23 @@ public class ShardingService {
         order.setUserId(UUID.randomUUID().toString().replaceAll("-", ""));
         myOrderMapper.insert(order);
     }
+
+    public String get() {
+        MyOrderCriteria myOrderCriteria = new MyOrderCriteria();
+        MyOrderCriteria.Criteria criteria = myOrderCriteria.createCriteria();
+        criteria.andOrderNoEqualTo("3fbeb0181c5747aa8efc5066c9b06c5f");
+        return JSON.toJSONString(myOrderMapper.selectByExample(myOrderCriteria));
+    }
+
+    public void insertGoods() {
+        Goods goods = new Goods();
+        goods.setGoodsId(UUID.randomUUID().toString().replaceAll("-", ""));
+        goods.setGoodsName("测试");
+        goodsMapper.insert(goods);
+    }
+
+    public String getGoods() {
+        return JSON.toJSONString(goodsMapper.selectByPrimaryKey(1));
+    }
+
 }
